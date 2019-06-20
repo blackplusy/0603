bf88d45479bb9d6d04a0e48fd399b65e014040d2
blackplusy <happyming07@sina.com>
2019-06-20 10:44:37 +0800

    Add files via upload

---
+++ 0620-01.python的输出.py
@@ -0,0 +1,24 @@
+#coding=utf-8
+#print()函数把信息输出到终端上
+#直接输出
+print(100)
+print('heygor is handsome!!!')
+#变量输出
+#a是变量的名字，100是变量的值
+a=100
+print(a)
+b=20
+print(a+b)
+#函数输出
+#abs()取绝对值
+#len()计算字符串长度
+print(abs(-1))
+name='simida'
+print(len(name))
+#格式化输出
+#%s    格式化显示字符类型数据
+#%d    格式化显示数字类型数据
+name='python'
+num=1
+print('%s is no.%d'%(name,num))
+print('your name is %s'%name)
---
+++ 0620-02.python的输入.py
@@ -0,0 +1,6 @@
+#coding=utf-8
+#接受键盘输入input()函数,括号中可以写入提示信息
+#name=input()
+#print('your name is %s'% name)
+name=input('请输入您的名字:')
+print('your name is %s'% name)
